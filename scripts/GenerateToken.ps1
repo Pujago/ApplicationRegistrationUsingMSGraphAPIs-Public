cffd083534ef9914539199c630e183d9796e56e7
Pujah Goviel <pujah.goviel@acc.co.nz>
2022-01-04 23:05:14 +1300

    Added comments

--- scripts/GenerateToken.ps1
+++ scripts/GenerateToken.ps1
@@ -6,6 +6,7 @@ function global:GenerateJWTToken {
         $clientAppRegistration = Get-AzureADMSApplication -Filter "DisplayName eq '$($displayName)'"
         $clientId = $clientAppRegistration.AppId           
         $url = "https://login.microsoftonline.com/<your tenant Id>/oauth2/token"
+        # Recommended to use key vaults to store app reg credentials and fetch from there
         $body = "grant_type=client_credentials&client_id=$clientId&client_secret=<your app registration secret>&resource=https://graph.microsoft.com"
         $header = @{
             "Content-Type" = 'application/x-www-form-urlencoded'
